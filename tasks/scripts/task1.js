93c65e8387c8b469c64825a35cd224bb5f708eb8
Eugen Kondratiev <kondratiev.es@gmail.com>
2018-12-29 22:43:11 +0200

    start Homework6

--- tasks/scripts/task1.js
+++ tasks/scripts/task1.js
@@ -1,1 +1,14 @@
 //task 6.1
+const paragraphs = document.getElementsByTagName("p");
+const paragraphsQ = document.querySelectorAll("p");
+console.log(paragraphs[1]);
+console.log(paragraphsQ.item(1));
+
+const firstDiv = document.querySelector("div");
+const needdParagraph = firstDiv.children.item(0).children.item(1);
+console.log(needdParagraph);
+
+
+
+
+
